4c01794151deb4d648493e16006a1fbc190b93d7
carlance <carlance123@gmail.com>
2016-04-20 16:23:21 +0100

    changed comments

--- Web Applications Architecture/week10/app/conf/config.inc.php
+++ Web Applications Architecture/week10/app/conf/config.inc.php
@@ -8,7 +8,7 @@ define("DB_NAME", "luca" ); 			// set database name
 define("DB_CHARSET", "utf8" ); 			// set database charset
 define("DB_DEBUGMODE", true ); 			// set database charset
 
-//gg
+//this is a comment
 
 /* actions for the USERS REST resource */
 define("ACTION_GET_USER", 33);
